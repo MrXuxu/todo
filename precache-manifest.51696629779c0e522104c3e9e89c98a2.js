ba9b6b5ddad35f5c2da429d4decb388a8e6f7133
xubaoshan <1411673883@qq.com>
2018-12-02 18:35:30 +0800

    完工

---
+++ precache-manifest.51696629779c0e522104c3e9e89c98a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2215b928f4f7cdc39ec",
+    "url": "assets/css/app.ca8e5aaa.css"
+  },
+  {
+    "revision": "c2215b928f4f7cdc39ec",
+    "url": "assets/js/app.65ff0d5b.js"
+  },
+  {
+    "revision": "df54b561821025beba97",
+    "url": "assets/js/chunk-vendors.33dde69c.js"
+  },
+  {
+    "revision": "91dd32f99825e5dcb7803107d9e858aa",
+    "url": "assets/img/img3.91dd32f9.jpg"
+  },
+  {
+    "revision": "177bb5bbfa4dbfc11c75385b442cf557",
+    "url": "assets/img/img4.177bb5bb.jpg"
+  },
+  {
+    "revision": "861f4258f19a4d2f15e3536cad2269e3",
+    "url": "assets/img/img2.861f4258.jpg"
+  },
+  {
+    "revision": "bf925694486d86642ac8838e681d6b9d",
+    "url": "assets/img/img.bf925694.jpg"
+  },
+  {
+    "revision": "1a7efb79f55892418422b0bdc4d007e1",
+    "url": "assets/img/img1.1a7efb79.jpg"
+  },
+  {
+    "revision": "b112f2b6a670403649c0251ef2adc097",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
